d898bfcc8b83df1a3fc591407b8871962313bf37
Mateusz Wasilewski <01158977@pw.edu.pl>
2021-03-15 20:58:35 +0100

    Combined code into one file named 'morse.py'.

--- morse.py
+++ morse.py
@@ -1,6 +1,7 @@
-from morse_io import read_data, write_data
+import argparse
+import sys
 
-
+word_separator = '/'
 morse_code = {
     'A': '.-',
     'B': '_...',
@@ -28,5 +29,38 @@ morse_code = {
     'X': '-..-',
     'Y': '-.--',
     'Z': '--..'
-
 }
+
+def morse(file_handle):
+    """
+    Function responsible for transforming given data into morse coded data.
+    """
+    for line in file_handle:
+        coded = ''
+        for sign in line:
+            sign = sign.capitalize()
+            if sign in morse_code:
+                coded += morse_code[sign]
+            elif sign == ' ' and coded[-1] != word_separator:
+                coded += word_separator
+        print(coded)
+
+
+def main(arguments):
+    """
+    Main funcion of the console ui.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('FILENAME')
+    args = parser.parse_args(arguments[1:])
+
+    try:
+        with open(args.FILENAME, 'r') as file_handle:
+            morse(file_handle)
+    except IsADirectoryError:
+        print('File is a directory.')
+    except FileNotFoundError:
+        print('File not found.')
+
+if __name__ == "__main__":
+    main(sys.argv)
