faa99173ea9aad939b758ef3985d93db74c8ec18
mwasilew <01158977@pw.edu.pl>
2021-04-16 08:47:38 +0200

    Updated class Node.

--- lab2_trees/node.py
+++ lab2_trees/node.py
@@ -1,8 +1,15 @@
 class Node:
-    def __init__(self, parent=None, left=None, right=None):
+    def __init__(self, value, parent=None, left=None, right=None):
+        self.set_value(value)
         self.set_parent(parent)
         self.set_left(left)
         self.set_right(right)
+
+    def set_value(self, value):
+        self._value = value
+
+    def get_value(self):
+        return self._value
 
     def set_parent(self, new_parent):
         self._parent = new_parent
--- lab2_trees/test_node.py
+++ lab2_trees/test_node.py
@@ -2,28 +2,32 @@ from node import Node
 
 
 def test_init_node_no_values():
-    nd = Node()
+    nd = Node(12)
+    assert nd.get_value() == 12
     assert nd.get_parent() is None
     assert nd.get_left() is None
     assert nd.get_right() is None
 
 
 def test_init_node_regular_node_case():
-    nd = Node(5, 1, 10)
+    nd = Node(12, 5, 1, 10)
+    assert nd.get_value() == 12
     assert nd.get_parent() == 5
     assert nd.get_left() == 1
     assert nd.get_right() == 10
 
 
 def test_init_node_root_node_case():
-    nd = Node(left=1, right=10)
+    nd = Node(12, left=1, right=10)
+    assert nd.get_value() == 12
     assert nd.get_parent() is None
     assert nd.get_left() == 1
     assert nd.get_right() == 10
 
 
 def test_init_node_leaf_node_case():
-    nd = Node(parent=10)
+    nd = Node(12, parent=10)
+    assert nd.get_value() == 12
     assert nd.get_parent() == 10
     assert nd.get_left() is None
     assert nd.get_right() is None
