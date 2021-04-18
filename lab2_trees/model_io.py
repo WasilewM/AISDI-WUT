083e33786fd2b7bdedd99ef32d10c9b24f46838b
mwasilew <01158977@pw.edu.pl>
2021-04-18 19:17:59 +0200

    Created files to test AVL and BST trees time performance.

---
+++ lab2_trees/model_io.py
@@ -0,0 +1,39 @@
+def read_data(file_handle):
+    data = []
+    for line in file_handle:
+        data.append(line) if line[-1] != '\n' else data.append(line[:-1])
+    return data
+
+
+def write_data(file_handle, data):
+    for line in data:
+        file_handle.write(line + '\n')
+
+
+def save_data(avl_results, bst_results):
+    try:
+        with open("create_trees_time_results.csv", 'w') as file_handle:
+            results = []
+            results.append(avl_results[0])
+            results.append(bst_results[0])
+            write_data(file_handle, results)
+    except IsADirectoryError:
+        print('Path is a directory.')
+
+    try:
+        with open("search_numbers_time_results.csv", 'w') as file_handle:
+            results = []
+            results.append(avl_results[0])
+            results.append(bst_results[0])
+            write_data(file_handle, results)
+    except IsADirectoryError:
+        print('Path is a directory.')
+
+    try:
+        with open("delete_numbers_time_results.csv", 'w') as file_handle:
+            results = []
+            results.append(avl_results[0])
+            results.append(bst_results[0])
+            write_data(file_handle, results)
+    except IsADirectoryError:
+        print('Path is a directory.')
