401ace258bc2772c56584267517d0f534e3a7fab
mwasilew <01158977@pw.edu.pl>
2021-05-03 10:29:20 +0200

    Updated Heap_Base.

--- lab3_heaps/heap_base.py
+++ lab3_heaps/heap_base.py
@@ -2,6 +2,7 @@ class Heap_Base:
     def __init__(self, dimension=2):
         self.set_dimension(dimension)
         self.heap = []
+        self.set_last()
 
     def set_dimension(self, dimension):
         if dimension >= 2:
@@ -11,3 +12,12 @@ class Heap_Base:
 
     def get_dimension(self):
         return self._dimension
+
+    def set_last(self, new_last=0):
+        if new_last >= 0:
+            self._last = new_last
+        else:
+            self._last = 0
+
+    def last(self):
+        return self._last
--- lab3_heaps/test_heap_base.py
+++ lab3_heaps/test_heap_base.py
@@ -5,15 +5,18 @@ def test_init_default():
     examplary_heap = Heap_Base()
     assert examplary_heap.get_dimension() == 2
     assert examplary_heap.heap == []
+    assert examplary_heap.last() == 0
 
 
 def test_init_given_dimension():
     examplary_heap = Heap_Base(4)
     assert examplary_heap.get_dimension() == 4
     assert examplary_heap.heap == []
+    assert examplary_heap.last() == 0
 
 
 def test_init_given_dimension_less_then_2():
     examplary_heap = Heap_Base(1)
     assert examplary_heap.get_dimension() == 2
     assert examplary_heap.heap == []
+    assert examplary_heap.last() == 0
