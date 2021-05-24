42fb669fe2178f9df0caba6bf01018936d55df10
mkowal21 <01158757@pw.edu.pl>
2021-05-24 17:01:31 +0200

    Added KR implementation with manual mapping and fixed bug

--- lab4_text_search/main.py
+++ lab4_text_search/main.py
@@ -3,6 +3,7 @@ from words import get_words
 from kmp import find_kmp
 from naive import find_naive
 from rabin_karp_pt import find_rk
+from rabin_karp_pt_map import find_rk_map
 from check_performance import check_performance
 
 
@@ -12,9 +13,11 @@ def run():
     result_kmp = check_performance(words, data, find_kmp)
     result_naive = check_performance(words, data, find_naive)
     result_rk = check_performance(words, data, find_rk)
-    print(result_kmp, result_naive, result_rk)
+    result_rk_map = check_performance(words, data, find_rk_map)
 
-    write_data([result_kmp, result_naive, result_rk])
-
+    print(result_kmp, result_naive, result_rk, result_rk_map)
+    # print(result_rk_map)
+    write_data([result_kmp, result_naive, result_rk, result_rk_map])
+    # write_data(result_rk_map)
 
 run()
